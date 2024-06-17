01e68644cab247d7f452ae78146035aedcf79f3f
Kristofer Peterson <svenski123@users.noreply.github.com>
2024-06-18 01:10:10 +0200

    Add support for freebsd. Replace deprecated type alias libc::uint64_tx with u64.

--- src/lib.rs
+++ src/lib.rs
@@ -34,12 +34,26 @@ mod imp {
 mod imp {
   #[link(name = "pthread")]
   extern "C" {
-    fn pthread_threadid_np(thread: libc::pthread_t, thread_id: *mut libc::uint64_t) -> libc::c_int;
+    fn pthread_threadid_np(thread: libc::pthread_t, thread_id: *mut u64) -> libc::c_int;
   }
 
   pub fn gettid() -> u64 {
     let mut result = 0;
     unsafe {let _ = pthread_threadid_np(0, &mut result); }
+    result
+  }
+}
+
+#[cfg(target_os = "freebsd")]
+mod imp {
+  #[link(name = "pthread")]
+  extern "C" {
+    fn pthread_getthreadid_np(thread: libc::pthread_t, thread_id: *mut u64) -> libc::c_int;
+  }
+
+  pub fn gettid() -> u64 {
+    let mut result = 0;
+    unsafe {let _ = pthread_getthreadid_np(0, &mut result); }
     result
   }
 }
